82de90b3ff8c58fa73271e0e6804e050ff5c0952
Artem Sychov <artem.suchov@gmail.com>
2019-07-15 19:42:08 +0300

    create some routers and fetch data with getInitialProps

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,34 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
+import fetch from "isomorphic-unfetch";
+import { Component } from "react";
 
-export default () => (
-  <Layout title="About">
-    <Link href="/">
-      <a>Go to home</a>
-    </Link>
-    <p>A JavaScript programmer</p>
-    <img src="/static/logo.svg" alt="JavaScript" height="200px" />
-  </Layout>
-);
+export default class About extends Component {
+  static async getInitialProps() {
+    const res = await fetch("https://api.github.com/users/suchov");
+    const data = await res.json();
+
+    return { user: data };
+  }
+
+  // componentDidMount() {
+  //   fetch("https://api.github.com/users/suchov")
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       this.setState({
+  //         user: data
+  //       });
+  //     });
+  // }
+
+  render() {
+    const { user } = this.props;
+
+    return (
+      <Layout title="About">
+        <p>{user.name}</p>
+        <img src={user.avatar_url} alt="Me" height="200px" />
+      </Layout>
+    );
+  }
+}
